d033f86b33ea08c12cf27c0f07969eb7c94df402
Namay Jindal <namayjindal@nyu.edu>
2025-05-02 11:12:48 -0400

    added a new item.html file with a button

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Web Dev Playground
+
+Made this public only so I could pass this into Claude and ask for some basic stuff. Repo's purpose is purely to mess around with some web dev fundamentals to practice doing the same thing on my class exam
--- public/script.js
+++ public/script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOMContent Fully loaded and parsed')
 })
+
+const addItemButton = document.getElementById('add-item');
+
+addItemButton.addEventListener('click', () => {
+    console.log('item added')
+})
